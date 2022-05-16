8ba0e4c85ddc6cc7d87ed89a7d68586452179d1f
Md Abrar Nasim <abrarnsim555@gmail.com>
2022-05-16 17:20:23 +0530

    add the code for printKeypadCombination

---
+++ recursion/printKeypadCombination.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cin>>num;
+    printKeypad(num);
+    return 0;
+}
+
+void printKeypadHelp(int n, string output) {
+    string keypad[] = {"abc","def", "ghi", "jkl", "mno","pqrs", "tuv", "wxyz"};
+    
+    
+    if(n==0) { // base case
+        cout<<output<<endl;
+        return;
+    }
+    
+    int k = n%10;
+    
+    if(k == 7 || k==9) {
+        printKeypadHelp(n/10,keypad[k-2][0]+output);
+        printKeypadHelp(n/10,keypad[k-2][1]+output);
+        printKeypadHelp(n/10,keypad[k-2][2]+output);
+        printKeypadHelp(n/10,keypad[k-2][3]+output);
+
+    }
+    
+    else {
+        printKeypadHelp(n/10,keypad[k-2][0]+output);
+        printKeypadHelp(n/10,keypad[k-2][1]+output);
+        printKeypadHelp(n/10,keypad[k-2][2]+output);
+        
+    }
+    
+    
+    
+}
+
+void printKeypad(int num){
+    /*
+    Given an integer number print all the possible combinations of the keypad. You do not need to return anything just print them.
+    */
+    printKeypadHelp(num,"");
+}
+
